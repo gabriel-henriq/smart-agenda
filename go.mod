925094dc6f72a0ee416fb457959ca70dfe1dfa25
Ivan Noleto <ivannoleto@hotmail.com>
2022-12-03 18:05:43 -0300

    add migrate in main

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/gin-gonic/gin v1.8.1
+	github.com/golang-migrate/migrate/v4 v4.15.2
 	github.com/lib/pq v1.10.7
 	github.com/spf13/viper v1.14.0
 )
@@ -15,6 +16,8 @@ require (
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.1 // indirect
 	github.com/goccy/go-json v0.9.11 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
@@ -31,6 +34,7 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.4.1 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
 	golang.org/x/crypto v0.3.0 // indirect
 	golang.org/x/net v0.2.0 // indirect
 	golang.org/x/sys v0.2.0 // indirect
--- main.go
+++ main.go
@@ -3,19 +3,31 @@ package main
 import (
 	"database/sql"
 	"github.com/gabriel-henriq/smart-agenda/api"
-	_ "github.com/lib/pq"
-
 	"github.com/gabriel-henriq/smart-agenda/db"
 	"github.com/gabriel-henriq/smart-agenda/util"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
 )
 
 func main() {
 	config, _ := util.LoadConfig(".")
 	conn, _ := sql.Open(config.DBDriver, config.DBSource)
+	driver, _ := postgres.WithInstance(conn, &postgres.Config{})
+
+	runDBMigration(config.MigrationURL, config.DBDriver, driver)
 
 	store := db.NewStore(conn)
 
 	runGinServer(config, store)
+}
+
+func runDBMigration(migrationURL string, dbSource string, driver database.Driver) {
+	m, _ := migrate.NewWithDatabaseInstance(migrationURL, dbSource, driver)
+
+	_ = m.Up()
 }
 
 func runGinServer(config util.Config, store db.Store) {
